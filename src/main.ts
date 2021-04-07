71e40b8504089267906045cc10ec7b9d096e5253
Nico Hülscher <nhuelscher@outlook.com>
2021-04-07 09:14:30 +0200

    Remove production things from local env
    # Conflicts:
    #	src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,39 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-// import { SocketIoAdapter } from './socket-io.adapter';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+const allowedOrigins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:9000",
+    "hyperloop.vercel.app",
+    "ewercard.vercel.app"
+    ];
+
+const corsOptions = {
+    origin: (origin: string, callback) => {
+        if (allowedOrigins.includes(origin) || !origin || allowedOrigins.findIndex(or => origin.endsWith(or) || origin.startsWith(or))) {
+            callback(null, true);
+        } else {
+            callback(new Error("Origin not allowed by CORS"));
+        }
+    }
+};
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
-  await app.listen(3000);
-  // app.useWebSocketAdapter(new SocketIoAdapter(app));
+    let appConfig = {};
+    if (process.env.PRODUCTION) {
+        const httpsOptions = {
+            cert: process.env.HTTPS_SERVER_CRT.replace(/\\n/gm, "\n"),
+            key: process.env.HTTPS_SERVER_KEY.replace(/\\n/gm, "\n"),
+        };
+        // prod config
+        appConfig = {
+            httpsOptions,
+        };
+    }
+    const app = await NestFactory.create(AppModule, appConfig);
+    app.enableCors(corsOptions);
+    await app.listen(3000);
 }
 bootstrap();
